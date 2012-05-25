7959c362096592d1bfd8742271cb59d34f1d196e
Tatsuki Yokomine <z.ibra.veste@gmail.com>
2012-05-25 19:54:06 +0900

    Update Config/database.php

--- Config/database.php
+++ Config/database.php
@@ -64,7 +64,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '1x60Ks4N',
+		'password' => '',
 		'database' => 'hondey',
 		'prefix' => '',
 		'encoding' => 'utf8',
